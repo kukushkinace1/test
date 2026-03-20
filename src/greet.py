7174344306c3fb4dcc6e176362beb5adf09cbc0a
ANTON <1999anton36@gmail.com>
2026-03-20 16:11:59 +0300

    fix1

--- src/greet.py
+++ src/greet.py
@@ -2,4 +2,4 @@ def greet(name: str) -> str:
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    print(greet("World")) # Должно вывести "Hello, World!"
+    print(greet("World")) # Должно вывести "Hello, World!"sdkfds
